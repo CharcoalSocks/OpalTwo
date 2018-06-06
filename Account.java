e32dc71232e3fb5cdef4ff3410c9fdf6e2c6794e
Alan Wen <asianhappydude@hotmail.com>
2018-06-07 02:36:13 +1000

    Added Main and Account classes
    Base files

---
+++ Account.java
@@ -0,0 +1,20 @@
+package examPrep;
+
+public class Account {
+	//name of account owner
+	private String ID;
+	//balamce of account
+	private int balance;
+	
+	public String getID() {
+		return ID;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public int transaction() {
+		return balance;
+	}
+}
